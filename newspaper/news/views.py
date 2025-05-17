868d9f99f281bc074e84165c8724dc5267dbc846
khamraevs <210115349+snrgtemp@users.noreply.github.com>
2025-05-17 12:19:30 +0500

    salom

--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -3,7 +3,7 @@ from .models import Post
 # Create your views here.
 
 def home(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-created_at')
     return render(request, 'news/base.html', {'posts': posts})
 
 
