18c2e1b4645ad5cb5d6a2a8f5edca15ba02f6c1c
khamraevs <210115349+snrgtemp@users.noreply.github.com>
2025-05-17 10:57:12 +0500

    salom

--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'news/base.html', {'posts': posts})
+
+
+
