71e17ede333b0ce8c630851c9ab9e572b4ef4a6e
khamraevs <210115349+snrgtemp@users.noreply.github.com>
2025-05-30 14:09:43 +0500

    30_05

--- newspaper/news/urls.py
+++ newspaper/news/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='domashniye_kotleti'),
+    path('topic/<nazvaniye_temi>/', views.filtr_po_temam, name='filtr_po_temam'),
 ]
--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -8,4 +8,10 @@ def home(request):
     return render(request, 'news/home.html', {'posts': posts, 'topics': topics})
 
 
+def filtr_po_temam(request, nazvaniye_temi):
+    posts = Post.objects.filter(topic=nazvaniye_temi).order_by('-created_at')
+    topics = Post.objects.values_list('topic', flat=True).distinct()
+    return render(request, 'news/home.html', {'posts': posts, 'topics': topics, 'nazvaniye_temi': nazvaniye_temi})
+
+
 
