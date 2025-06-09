59f07f79f8bfbecd8140e8093e6004f231f62ee1
khamraevs <210115349+snrgtemp@users.noreply.github.com>
2025-06-09 11:58:19 +0500

    salom

--- newspaper/news/models.py
+++ newspaper/news/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
+from autoslug import AutoSlugField
+
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from='title', unique=True)
     content = models.TextField()
     topic = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.title
--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -5,16 +5,23 @@ from .models import Post
 def home(request):
     posts = Post.objects.all().order_by('-created_at')
     topics = Post.objects.values_list('topic', flat=True).distinct()
-    return render(request, 'news/home.html', {'posts': posts, 'topics': topics})
+    return render(request, 'news/home.html', {  'posts': posts, 
+                                                'topics': topics})
 
 
 def filtr_po_temam(request, nazvaniye_temi):
     posts = Post.objects.filter(topic=nazvaniye_temi).order_by('-created_at')
     topics = Post.objects.values_list('topic', flat=True).distinct()
-    return render(request, 'news/home.html', {'posts': posts, 'topics': topics, 'nazvaniye_temi': nazvaniye_temi})
+    return render(request, 'news/home.html', {  'posts': posts, 
+                                                'topics': topics, 
+                                                'nazvaniye_temi': nazvaniye_temi})
 
 def otkrit_post(request, post_title):
     # post = get_object_or_404(Post, title=post_title)
     post = Post.objects.get(title=post_title)
-    return render(request, 'news/post.html', {'post': post})
+    topics = Post.objects.values_list('topic', flat=True).distinct()
+    return render(request, 'news/post.html', {  'post': post, 
+                                                'topics': topics, 
+                                                "post_title": post_title})
+                                                
 
