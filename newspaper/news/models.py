c7f25a107daadce838dbd21bc05a691e97f82a8a
khamraevs <210115349+snrgtemp@users.noreply.github.com>
2025-06-13 10:43:35 +0500

    salom

--- newspaper/news/models.py
+++ newspaper/news/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.utils.text import slugify
 from autoslug import AutoSlugField
+from ckeditor.fields import RichTextField
 
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    slug = AutoSlugField(populate_from='title', unique=True)
-    content = models.TextField()
+    slug = AutoSlugField(populate_from='title', unique=True, editable=True)
+    content = RichTextField()
     topic = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
--- newspaper/news/urls.py
+++ newspaper/news/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.home, name='domashniye_kotleti'),
     path('topic/<nazvaniye_temi>/', views.filtr_po_temam, name='filtr_po_temam'),
-    path('topic/<post_title>', views.otkrit_post, name='otkrit_post'),
+    path('topic/<post_slug>', views.otkrit_post, name='otkrit_post'),
+
 ]
